3eb2e6cca98132a264c876eb49f6543a326d660c
librauee <870407139@qq.com>
2019-08-02 15:22:44 +0800

    SRM34671维特征提取与FLD集成分类器

---
+++ 富模型空域图像隐写分析(SRM)/提取特征/SubmodelConcatenation.m
@@ -0,0 +1,12 @@
+function fea = SubmodelConcatenation(F)
+Ss = fieldnames(F);
+indexTo = 0;
+    for Sid = 1:length(Ss)
+        Fsingle = eval(['F.' Ss{Sid}]);
+        indexFrom = indexTo + 1;
+%         fprintf('   F.%s : %d x %d\n', Ss{Sid}, size(Fsingle, 1), size(Fsingle, 2));
+        indexTo = indexFrom + size(Fsingle,2) - 1;
+        fea(1,indexFrom:indexTo) = Fsingle;
+    end
+end
+
